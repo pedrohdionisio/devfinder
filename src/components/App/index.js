4e59ec3dd31d6426e856928e131fb5f7ef994f4d
Pedro Henrique Dionisio <pedrohenriquedionisioalves@gmail.com>
2021-12-23 22:57:17 -0300

    feat: stylizes main container

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,12 +3,16 @@ import { ThemeProvider } from 'styled-components'
 import darkTheme from '../../assets/styles/themes/dark'
 
 import GlobalStyles from '../../assets/styles/global'
+import { Wrapper } from './styles'
+import Header from '../Header'
 
 export default function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <GlobalStyles />
-      App
+      <Wrapper>
+        <Header />
+      </Wrapper>
     </ThemeProvider>
   )
 }
---
+++ src/components/App/styles.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+export const Wrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+
+  max-width: 700px;
+  width: 100%;
+  height: 100vh;
+
+  margin: 0 auto;
+`
